68d76245344ac1c633a86fba488941e3767f4693
叱咤月海鱼鱼猫 <ishuiliyi@gmail.com>
2025-10-25 16:18:04 +0800

    Refactor AboutActivity to utilize string resources for improved localization and consistency. This change enhances the user experience by ensuring UI components are more adaptable to different languages.

--- app/src/main/java/com/stfreya/frpc/AboutActivity.kt
+++ app/src/main/java/com/stfreya/frpc/AboutActivity.kt
@@ -19,10 +19,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.stfreya.frpc.R
 import com.stfreya.frpc.ui.theme.StfreyaFrpcTheme
 
 class AboutActivity : ComponentActivity() {
